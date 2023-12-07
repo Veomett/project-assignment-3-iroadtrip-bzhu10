430ac0135ba688a0fb31ea44a3053df6adc8b9ad
bzhu10 <98488583+bzhu10@users.noreply.github.com>
2023-12-07 12:12:45 +0800

    Create Capdist.java

---
+++ Capdist.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Capdist{
+    public static Integer numa;
+    public static String ida;
+    public List<CapdistSon> capdistSons;
+
+    public Capdist getCapdist(String idaTest){
+        ida = idaTest;
+
+        return null;
+    }
+}
